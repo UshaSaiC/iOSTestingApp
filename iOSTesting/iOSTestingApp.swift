4da1652583d2e8967688eb8cc680cc37e01b614f
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-21 11:05:45 +0530

    Initial Commit

---
+++ iOSTesting/iOSTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestingApp.swift
+//  iOSTesting
+//
+//  Created by Usha Sai Chintha on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
